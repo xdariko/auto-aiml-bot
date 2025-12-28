186277d675084a512ed0ebc3b749a78283cb4936
Daria-Romanko <Diagic1000@gmail.com>
2025-12-28 23:30:01 +0300

    Add proj

---
+++ AIMLService.cs
@@ -0,0 +1,39 @@
+﻿using AIMLbot;
+using System.Collections.Generic;
+
+namespace AIMLTGBot
+{
+    public class AIMLService
+    {
+        readonly Bot bot;
+        readonly Dictionary<long, User> users = new Dictionary<long, User>();
+
+        public AIMLService()
+        {
+            bot = new Bot();
+            bot.loadSettings();
+            bot.isAcceptingUserInput = false;
+            bot.loadAIMLFromFiles();
+            bot.isAcceptingUserInput = true;
+        }
+
+        public string Talk(long userId, string userName, string phrase)
+        {
+            var result = "";
+            User user;
+            if (!users.ContainsKey(userId))
+            {
+                user = new User(userId.ToString(), bot);
+                users.Add(userId, user);
+                Request r = new Request($"Меня зовут {userName}", user, bot);
+                result += bot.Chat(r).Output + System.Environment.NewLine;
+            }
+            else
+            {
+                user = users[userId];
+            }
+            result += bot.Chat(new Request(phrase, user, bot)).Output;
+            return result;
+        }
+    }
+}
---
+++ TelegramService.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace AIMLTGBot
+{
+    public class TelegramService : IDisposable
+    {
+        private readonly TelegramBotClient client;
+        private readonly AIMLService aiml;
+        // CancellationToken - инструмент для отмены задач, запущенных в отдельном потоке
+        private readonly CancellationTokenSource cts = new CancellationTokenSource();
+        public string Username { get; }
+
+        public TelegramService(string token, AIMLService aimlService)
+        {
+            aiml = aimlService;
+            client = new TelegramBotClient(token);
+            client.StartReceiving(HandleUpdateMessageAsync, HandleErrorAsync, new ReceiverOptions
+            {   // Подписываемся только на сообщения
+                AllowedUpdates = new[] { UpdateType.Message }
+            },
+            cancellationToken: cts.Token);
+            // Пробуем получить логин бота - тестируем соединение и токен
+            Username = client.GetMeAsync().Result.Username;
+        }
+
+        async Task HandleUpdateMessageAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            var message = update.Message;
+            var chatId = message.Chat.Id;
+            var username = message.Chat.FirstName;
+            if (message.Type == MessageType.Text)
+            {
+                var messageText = update.Message.Text;
+
+                Console.WriteLine($"Received a '{messageText}' message in chat {chatId} with {username}.");
+
+                // Echo received message text
+                await botClient.SendTextMessageAsync(
+                    chatId: chatId,
+                    text: aiml.Talk(chatId, username, messageText),
+                    cancellationToken: cancellationToken);
+                return;
+            }
+            // Загрузка изображений пригодится для соединения с нейросетью
+            if (message.Type == MessageType.Photo)
+            {
+                var photoId = message.Photo.Last().FileId;
+                Telegram.Bot.Types.File fl = await client.GetFileAsync(photoId, cancellationToken: cancellationToken);
+                var imageStream = new MemoryStream();
+                await client.DownloadFileAsync(fl.FilePath, imageStream, cancellationToken: cancellationToken);
+                // Если бы мы хотели получить битмап, то могли бы использовать new Bitmap(Image.FromStream(imageStream))
+                // Но вместо этого пошлём картинку назад
+                // Стрим помнит последнее место записи, мы же хотим теперь прочитать с самого начала
+                imageStream.Seek(0, 0);
+                await client.SendPhotoAsync(
+                    message.Chat.Id,
+                    imageStream,
+                    "Пока что я не знаю, что делать с картинками, так что держи обратно",
+                    cancellationToken: cancellationToken
+                );
+                return;
+            }
+            // Можно обрабатывать разные виды сообщений, просто для примера пробросим реакцию на них в AIML
+            if (message.Type == MessageType.Video)
+            {
+                await client.SendTextMessageAsync(message.Chat.Id, aiml.Talk(chatId, username, "Видео"), cancellationToken: cancellationToken);
+                return;
+            }
+            if (message.Type == MessageType.Audio)
+            {
+                await client.SendTextMessageAsync(message.Chat.Id, aiml.Talk(chatId, username, "Аудио"), cancellationToken: cancellationToken);
+                return;
+            }
+        }
+
+        Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            var apiRequestException = exception as ApiRequestException;
+            if (apiRequestException != null)
+                Console.WriteLine($"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}");
+            else
+                Console.WriteLine(exception.ToString());
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            // Заканчиваем работу - корректно отменяем задачи в других потоках
+            // Отменяем токен - завершатся все асинхронные таски
+            cts.Cancel();
+        }
+    }
+}
